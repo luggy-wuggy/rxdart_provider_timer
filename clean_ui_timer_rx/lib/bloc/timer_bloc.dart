976003f5063f1507509c8161f5b00800ad77b072
luggy <luggyman123@gmail.com>
2021-10-12 13:07:37 -0700

    toggleRoundBreak function

--- clean_ui_timer_rx/lib/bloc/timer_bloc.dart
+++ clean_ui_timer_rx/lib/bloc/timer_bloc.dart
@@ -11,12 +11,15 @@ class TimerBloc {
   late BehaviorSubject<String> _subjectSetsDisplay;
 
   late BehaviorSubject<bool> _subjectTimerIsPlaying;
+  late BehaviorSubject<bool> _subjectTimerIsRound;
 
-  String initialTimeDisplay = "3:00";
-  String initialRoundTimeDisplay = "3:00";
-  String initialBreakTimeDisplay = "1:00";
-  String initialSetsDisplay = "12";
-  String initialTotalTimeDisplay = "48:00";
+  String initialTimeDisplay = "0:07";
+  String initialRoundTimeDisplay = "0:07";
+  String initialBreakTimeDisplay = "0:04";
+  String initialSetsDisplay = "3";
+  String initialTotalTimeDisplay = "0:30";
+
+  int roundCounter = 1;
 
   TimerBloc() {
     _subjectTimeDisplay = BehaviorSubject<String>.seeded(initialTimeDisplay);
@@ -28,6 +31,7 @@ class TimerBloc {
         BehaviorSubject<String>.seeded(initialTotalTimeDisplay);
     _subjectSetsDisplay = BehaviorSubject<String>.seeded(initialSetsDisplay);
     _subjectTimerIsPlaying = BehaviorSubject<bool>.seeded(false);
+    _subjectTimerIsRound = BehaviorSubject<bool>.seeded(false);
   }
 
   Stream<String> get timeObservable => _subjectTimeDisplay.stream;
@@ -36,9 +40,11 @@ class TimerBloc {
   Stream<String> get totalTimeObservable => _subjectTotalTimeDisplay.stream;
   Stream<String> get setObservable => _subjectSetsDisplay.stream;
   Stream<bool> get isPlayingObservable => _subjectTimerIsPlaying.stream;
+  Stream<bool> get isTimerRound => _subjectTimerIsRound.stream;
 
   void startTimer() {
     _subjectTimerIsPlaying.value = true;
+    _subjectTimerIsRound.sink.add(true);
     _timer.start();
     _startTimer();
   }
@@ -52,33 +58,12 @@ class TimerBloc {
     Timer(const Duration(seconds: 1), _keepRunning);
   }
 
-  void _keepRunning() {
+  void _keepRunning() async {
+    await _elapseTime();
+
     if (_timer.isRunning) {
       _startTimer();
     }
-
-    _elapseTime();
-  }
-
-  void _elapseTime() {
-    Duration displayTime = Duration(
-            minutes: int.parse(_subjectTimeDisplay.value.split(":")[0]),
-            seconds: int.parse(_subjectTimeDisplay.value.split(":")[1])) -
-        const Duration(seconds: 1);
-
-    Duration displayTotalTime = Duration(
-            minutes: int.parse(_subjectTotalTimeDisplay.value.split(":")[0]),
-            seconds: int.parse(_subjectTotalTimeDisplay.value.split(":")[1])) -
-        const Duration(seconds: 1);
-
-    final String stringDisplayTime =
-        "${(displayTime.inMinutes % 60).toString()}:${(displayTime.inSeconds % 60).toString().padLeft(2, '0')}";
-
-    final String stringTotalTime =
-        "${(displayTotalTime.inMinutes % 60).toString()}:${(displayTotalTime.inSeconds % 60).toString().padLeft(2, '0')}";
-
-    _subjectTimeDisplay.sink.add(stringDisplayTime);
-    _subjectTotalTimeDisplay.sink.add(stringTotalTime);
   }
 
   void setSet(String s) {
@@ -99,6 +84,49 @@ class TimerBloc {
 
   void toggleIsPlaying() {
     _subjectTimerIsPlaying.value = !(_subjectTimerIsPlaying.value);
+  }
+
+  Future<void> _toggleRoundBreak() async {
+    /// Round to Break
+    if (_subjectTimerIsRound.value) {
+      if (_subjectTimeDisplay.value == '0:00') {
+        _subjectTimerIsRound.sink.add(!_subjectTimerIsRound.value);
+        _subjectTimeDisplay.sink.add(_subjectBreakTimeDisplay.value);
+        await Future.delayed(Duration(seconds: 1));
+      }
+    }
+
+    /// Break to Round
+    else {
+      if (_subjectTimeDisplay.value == '0:00') {
+        _subjectTimerIsRound.sink.add(!_subjectTimerIsRound.value);
+        _subjectTimeDisplay.sink.add(_subjectRoundTimeDisplay.value);
+        await Future.delayed(Duration(seconds: 1));
+      }
+    }
+  }
+
+  Future<void> _elapseTime() async {
+    await _toggleRoundBreak();
+
+    Duration displayTime = Duration(
+            minutes: int.parse(_subjectTimeDisplay.value.split(":")[0]),
+            seconds: int.parse(_subjectTimeDisplay.value.split(":")[1])) -
+        const Duration(seconds: 1);
+
+    Duration displayTotalTime = Duration(
+            minutes: int.parse(_subjectTotalTimeDisplay.value.split(":")[0]),
+            seconds: int.parse(_subjectTotalTimeDisplay.value.split(":")[1])) -
+        const Duration(seconds: 1);
+
+    final String stringDisplayTime =
+        "${(displayTime.inMinutes % 60).toString()}:${(displayTime.inSeconds % 60).toString().padLeft(2, '0')}";
+
+    final String stringTotalTime =
+        "${(displayTotalTime.inMinutes % 60).toString()}:${(displayTotalTime.inSeconds % 60).toString().padLeft(2, '0')}";
+
+    _subjectTimeDisplay.sink.add(stringDisplayTime);
+    _subjectTotalTimeDisplay.sink.add(stringTotalTime);
   }
 
   void _updateTotalTime() {
