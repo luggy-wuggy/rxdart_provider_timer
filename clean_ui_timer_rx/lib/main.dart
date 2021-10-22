7e4209a72f05b90d9ba8dc2a0dc8343444ea4c1c
luggy <luggyman123@gmail.com>
2021-10-22 11:32:01 -0700

    added adsenses to app

--- clean_ui_timer_rx/lib/main.dart
+++ clean_ui_timer_rx/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 
 import 'package:clean_ui_timer_rx/screens/home.dart';
 import 'bloc/timer_provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
--- clean_ui_timer_rx/lib/screens/home.dart
+++ clean_ui_timer_rx/lib/screens/home.dart
@@ -1,9 +1,11 @@
+import 'package:clean_ui_timer_rx/model/ad_helper.dart';
 import 'package:clean_ui_timer_rx/widgets/bottom_bar.dart';
 import 'package:clean_ui_timer_rx/widgets/header.dart';
 import 'package:clean_ui_timer_rx/widgets/subheader.dart';
 import 'package:clean_ui_timer_rx/widgets/timer.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,9 +15,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late BannerAd _bottomBannerAd;
+  bool _isBottomBannerAdLoaded = false;
+
+  void _createBottomBannerAd() {
+    _bottomBannerAd = BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      size: AdSize.banner,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBottomBannerAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+        },
+      ),
+    );
+    _bottomBannerAd.load();
+  }
+
   @override
   void dispose() {
     super.dispose();
+    _bottomBannerAd.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _createBottomBannerAd();
   }
 
   @override
@@ -40,6 +71,13 @@ class _HomeState extends State<Home> {
           ],
         ),
       ),
+      bottomNavigationBar: _isBottomBannerAdLoaded
+          ? SizedBox(
+              height: _bottomBannerAd.size.height.toDouble(),
+              width: _bottomBannerAd.size.width.toDouble(),
+              child: AdWidget(ad: _bottomBannerAd),
+            )
+          : null,
     );
   }
 }
